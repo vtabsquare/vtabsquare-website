61a6842ddbdf1fa3da06c4e30b114492edc9d0f5
Harish-K499 <harishkadhi18022001@gmail.com>
2026-02-02 16:46:07 +0530

    Use Font Awesome CDN for icons

--- gaur/application/1.2.0/Views/app/default/common/css.php
+++ gaur/application/1.2.0/Views/app/default/common/css.php
@@ -2,5 +2,5 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@100;200;300;400;500;600;700;800&family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/app.min.css" />
-    <link rel="stylesheet" href="assets/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
